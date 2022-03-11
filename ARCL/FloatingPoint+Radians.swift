66cd1e1f8a702df501e221e331d83bb63050c47e
bibinurzh <49501369+bibinurzh@users.noreply.github.com>
2022-03-11 21:50:20 +0600

    Add files via upload

---
+++ ARCL/FloatingPoint+Radians.swift
@@ -0,0 +1,14 @@
+//
+//  FloatingPoint+Radians.swift
+//  AugmentedReality
+//
+//  Created by Bibinur on 11/9/19.
+//  Copyright © 2019 Bibinur. All rights reserved.
+//
+
+import Foundation
+
+public extension FloatingPoint {
+    public var degreesToRadians: Self { return self * .pi / 180 }
+    public var radiansToDegrees: Self { return self * 180 / .pi }
+}
